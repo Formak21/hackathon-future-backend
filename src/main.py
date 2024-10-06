2ee1c73dba05d996d6a0b1d4794db1742bdf9f87
Alexandr Formakovskiy <Form49.d@gmail.com>
2024-10-06 03:36:47 +0300

    Factory backgit add *git add *

--- src/main.py
+++ src/main.py
@@ -3,15 +3,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://user:pass@localhost:5434/dbtest"
+from src.factory import app
 
-
-class Base(DeclarativeBase):
-    pass
-
-
-db = SQLAlchemy(app, model_class=Base)
+db = SQLAlchemy(app, model_class=DeclarativeBase)
 dbModel = db.Model
 
 
