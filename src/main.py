a0840f773ec6bdfec06c8abda9a01b309e97c35e
Alexandr Formakovskiy <Form49.d@gmail.com>
2024-10-06 04:12:20 +0300

    main update

--- src/main.py
+++ src/main.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 
 from src.factory import app, Base, db
 
 if __name__ == "__main__":
 
-
     with app.app_context():
-        from src.model.user import User
-        from src.model.session import Session
+        from src.model import User, Session
+
         db.create_all()
 
         db.session.commit()
